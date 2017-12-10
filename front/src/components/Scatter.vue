b7567c6074cb4a3e30061398bca71698737f24db
Serg Kom <sergey.komarenko@infinum.hr>
2017-12-10 15:01:07 +0200

    Add dynamic

--- front/src/components/Scatter.vue
+++ front/src/components/Scatter.vue
@@ -3,13 +3,11 @@
     <h3 class="card-title" @click="showChart = !showChart">{{title}}</h3>
     <div class="chart" v-show="showChart">
       <b-row>
-        <b-col cols='4'><b-form-select v-model="changedSecondRow" :options="rows"></b-form-select></b-form-select></b-col>
-        <b-col cols='4'><b-form-select v-model="changedFirstRow" :options="rows"></b-form-select></b-col>
-        <b-btn v-on:click="applyRows">Apply</b-btn>
+        <b-col cols='4'><b-form-select v-model="secondRow" :options="rows"></b-form-select></b-form-select></b-col>
+        <b-col cols='4'><b-form-select v-model="firstRow" :options="rows"></b-form-select></b-col>
       </b-row>
       <br>
-      <scatter-chart v-if="pure" :data="pureData" :xtitle="firstRow" :ytitle="secondRow"></scatter-chart>
-      <scatter-chart v-if="!pure" :data="coloredData" :xtitle="firstRow" :ytitle="secondRow"></scatter-chart>
+      <scatter-chart :data="data" :xtitle="firstRow" :ytitle="secondRow"></scatter-chart>
     </div>
   </div>
 </template>
@@ -22,10 +20,7 @@ export default {
       showChart: true,
       firstRow: this.$store.state.rowNames[0],
       secondRow: this.$store.state.rowNames[1],
-      changedFirstRow: this.$store.state.rowNames[0],
-      changedSecondRow: this.$store.state.rowNames[1],
       pointsColors: ['red', 'green', 'blue', 'black', 'yellow', 'purple'],
-      chartData: null,
       rows: this.$store.state.rowNames,
     };
   },
@@ -39,24 +34,21 @@ export default {
     coloredData() {
       return this.chartData || this.processColors(this.chartRows);
     },
-  },
-  methods: {
-    applyRows() {
-      this.firstRow = this.changedFirstRow;
-      this.secondRow = this.changedSecondRow;
-      const firstIndex = this.rows.indexOf(this.changedFirstRow);
-      const secondIndex = this.rows.indexOf(this.changedSecondRow);
+    data() {
+      const firstIndex = this.rows.indexOf(this.firstRow);
+      const secondIndex = this.rows.indexOf(this.secondRow);
       const newArr = this.pure ? [firstIndex, secondIndex] : [0, firstIndex + 1, secondIndex + 1];
 
       if (this.pure) {
         const data = this.chartRows.map(el => el.slice(1, el.length));
-        this.chartData = data.map(el => newArr.map(it => el[it]));
-      } else {
-        const data = this.chartRows;
-        const toProcess = data.map(el => newArr.map(it => el[it]));
-        this.chartData = this.processColors(toProcess);
+        return data.map(el => newArr.map(it => el[it]));
       }
+      const data = this.chartRows;
+      const toProcess = data.map(el => newArr.map(it => el[it]));
+      return this.processColors(toProcess);
     },
+  },
+  methods: {
     processColors(data) {
       const groups = {};
       data.forEach((el) => {
